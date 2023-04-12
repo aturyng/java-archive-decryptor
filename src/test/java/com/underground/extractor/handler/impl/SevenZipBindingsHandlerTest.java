b5d06901a513aa2fde8d5a477a6d93c1368feb96
turyng <turyng@proton.me>
2023-04-12 13:19:01 -0400

    - extract main logic in tests to reuse and reduce boilerplate code  - add tests for archives with multiple files inside

--- src/test/java/com/underground/extractor/handler/impl/SevenZipBindingsHandlerTest.java
+++ src/test/java/com/underground/extractor/handler/impl/SevenZipBindingsHandlerTest.java
@@ -32,130 +32,139 @@ class SevenZipBindingsHandlerTest {
     }
 
     @AfterEach
-    void tearDown() {
-
+    void tearDown() throws IOException {
+        FileUtils.cleanDirectory(tempDir.toFile());
     }
 
-    @Test
-    void extractAll_pw_prot_zip_single_file_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+
+    private enum FileType {
+        SINGLE, MULTIPLE, SINGLE_ENCR_LIST
+    }
+
+    private String getArchiveMask(FileType type){
+        return switch (type) {
+            case SINGLE -> "single_file_inside.%s";
+            case MULTIPLE -> "multiple_files_inside.%s";
+            case SINGLE_ENCR_LIST -> "encr_file_list-single_file.%s";
+        };
+    }
+
+    private String getFileInsideMask(FileType type){
+        return switch (type) {
+            case SINGLE -> "%s_single_file.txt";
+            case MULTIPLE -> "%s_file_%d.txt";
+            case SINGLE_ENCR_LIST -> "%s_encr_file_list-single_file.txt";
+        };
+    }
+
+
+    private String getFileContentMask(FileType type){
+        return switch (type) {
+            case SINGLE -> "%s_single_file_content";
+            case MULTIPLE -> "%s_file_%d_content";
+            case SINGLE_ENCR_LIST -> "%s_encr_file_list-single_file_content";
+        };
+    }
+    private void doTestWithFilesInside(String format, int nrFilesInside, FileType type) throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
         //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.zip").getFile());
+        File archiveFile = new File(classLoader.getResource(String.format("test_data/" + getArchiveMask(type), format)).getFile());
 
         //WHEN
         boolean extractionResult = this.handler.extractAll(archiveFile, archivePassword, tempDir.toAbsolutePath().toString());
 
         //THEN
         assertTrue(extractionResult, "Should return true");
-        File extractedFile = tempDir.resolve("zip_single_file.txt").toFile();
-        assertTrue(extractedFile.exists(), "Extracted file should exist");
-        List<String> lines = FileUtils.readLines(extractedFile, Charset.defaultCharset());
-        assertEquals(1, lines.size(), "Archived txt file should contain only one line");
-        assertEquals("zip_single_file_content", lines.get(0), "Should contain predefined content");
+        for (int i = 1; i <= nrFilesInside; i++) {
+            File extractedFile = tempDir.resolve(String.format(getFileInsideMask(type), format, i)).toFile();
+            assertTrue(extractedFile.exists(), "Extracted file should exist");
+            List<String> lines = FileUtils.readLines(extractedFile, Charset.defaultCharset());
+            assertEquals(1, lines.size(), "Archived txt file should contain only one line");
+            assertEquals(String.format(String.format(getFileContentMask(type), format, i)), lines.get(0), "Should contain predefined content");
+        }
+    }
 
+    private void doTestWithMultipleFilesInside(String format) throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithFilesInside(format, 2, FileType.MULTIPLE);
+    }
+
+
+    @Test
+    void extractAll_pw_prot_zip_single_file_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithFilesInside("zip", 1, FileType.SINGLE);
     }
 
     @Test
     void extractAll_pw_prot_7z_single_file_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.7z").getFile());
-
-        //WHEN
-        boolean extractionResult = this.handler.extractAll(archiveFile, archivePassword, tempDir.toAbsolutePath().toString());
-
-        //THEN
-        assertTrue(extractionResult, "Should return true");
-        File extractedFile = tempDir.resolve("7z_single_file.txt").toFile();
-        assertTrue(extractedFile.exists(), "Extracted file should exist");
-        List<String> lines = FileUtils.readLines(extractedFile, Charset.defaultCharset());
-        assertEquals(1, lines.size(), "Archived txt file should contain only one line");
-        assertEquals("7z_single_file_content", lines.get(0), "Should contain predefined content");
-
+        this.doTestWithFilesInside("7z", 1, FileType.SINGLE);
     }
 
     @Test
     void extractAll_pw_prot_7z_encr_file_list_single_file_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/encr_file_list-single_file.7z").getFile());
-
-        //WHEN
-        boolean extractionResult = this.handler.extractAll(archiveFile, archivePassword, tempDir.toAbsolutePath().toString());
-
-        //THEN
-        assertTrue(extractionResult, "Should return true");
-        File extractedFile = tempDir.resolve("7z_encr_file_list-single_file.txt").toFile();
-        assertTrue(extractedFile.exists(), "Extracted file should exist");
-        List<String> lines = FileUtils.readLines(extractedFile, Charset.defaultCharset());
-        assertEquals(1, lines.size(), "Archived txt file should contain only one line");
-        assertEquals("7z_encr_file_list-single_file_content", lines.get(0), "Should contain predefined content");
-
+        this.doTestWithFilesInside("7z", 1, FileType.SINGLE_ENCR_LIST);
     }
 
     @Test
     void extractAll_pw_prot_rar_single_file_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithFilesInside("rar", 1, FileType.SINGLE);
+    }
+
+    @Test
+    void extractAll_pw_prot_7z_multiple_files_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithMultipleFilesInside("7z");
+    }
+
+    @Test
+    void extractAll_pw_prot_zip_multiple_files_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithMultipleFilesInside("zip");
+    }
+    @Test
+    void extractAll_pw_prot_rar_multiple_files_inside() throws ZipHandler7ZipImpl.ExtractionException, WrongPassException, IOException {
+        this.doTestWithMultipleFilesInside("rar");
+    }
+
+    private void doTestWrongPassException(String format, FileType type){
         //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.rar").getFile());
+        File archiveFile = new File(classLoader.getResource(String.format("test_data/" + getArchiveMask(type), format)).getFile());
 
         //WHEN
-        boolean extractionResult = this.handler.extractAll(archiveFile, archivePassword, tempDir.toAbsolutePath().toString());
-
-        //THEN
-        assertTrue(extractionResult, "Should return true");
-        File extractedFile = tempDir.resolve("rar_single_file.txt").toFile();
-        assertTrue(extractedFile.exists(), "Extracted file should exist");
-        List<String> lines = FileUtils.readLines(extractedFile, Charset.defaultCharset());
-        assertEquals(1, lines.size(), "Archived txt file should contain only one line");
-        assertEquals("rar_single_file_content", lines.get(0), "Should contain predefined content");
-
+        assertThrows(WrongPassException.class, () -> {
+            boolean extractionResult = this.handler.extractAll(archiveFile, wrongArchivePassword, tempDir.toAbsolutePath().toString());
+            //THEN
+            assertFalse(extractionResult);
+        }, "WrongPassException was expected");
     }
 
     @Test()
     void extractAll_pw_prot_zip_single_file_inside_wrong_pass() {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.zip").getFile());
-
-        //WHEN
-        assertThrows(WrongPassException.class, () -> {
-            boolean extractionResult = this.handler.extractAll(archiveFile, wrongArchivePassword, tempDir.toAbsolutePath().toString());
-            //THEN
-            assertFalse(extractionResult);
-        }, "WrongPassException was expected");
+        doTestWrongPassException("zip", FileType.SINGLE);
     }
 
     @Test()
     void extractAll_pw_prot_7z_single_file_inside_wrong_pass() {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.7z").getFile());
-
-        //WHEN
-        assertThrows(WrongPassException.class, () -> {
-            boolean extractionResult = this.handler.extractAll(archiveFile, wrongArchivePassword, tempDir.toAbsolutePath().toString());
-            //THEN
-            assertFalse(extractionResult);
-        }, "WrongPassException was expected");
+        doTestWrongPassException("7z", FileType.SINGLE);
     }
 
     @Test()
     void extractAll_pw_prot_7z_encr_file_list_single_file_inside_wrong_pass() {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/encr_file_list-single_file.7z").getFile());
-
-        //WHEN
-        assertThrows(WrongPassException.class, () -> {
-            boolean extractionResult = this.handler.extractAll(archiveFile, wrongArchivePassword, tempDir.toAbsolutePath().toString());
-            //THEN
-            assertFalse(extractionResult);
-        }, "WrongPassException was expected");
+        doTestWrongPassException("7z", FileType.SINGLE_ENCR_LIST);
     }
     @Test()
     void extractAll_pw_prot_rar_single_file_inside_wrong_pass() {
-        //GIVEN
-        File archiveFile = new File(classLoader.getResource("test_data/single_file_inside.rar").getFile());
+        doTestWrongPassException("rar", FileType.SINGLE);
+    }
 
-        //WHEN
-        assertThrows(WrongPassException.class, () -> {
-            boolean extractionResult = this.handler.extractAll(archiveFile, wrongArchivePassword, tempDir.toAbsolutePath().toString());
-            //THEN
-            assertFalse(extractionResult);
-        }, "WrongPassException was expected");
+    @Test()
+    void extractAll_pw_prot_zip_multiple_files_inside_wrong_pass() {
+        doTestWrongPassException("zip", FileType.MULTIPLE);
+    }
+
+    @Test()
+    void extractAll_pw_prot_7z_multiple_files_inside_wrong_pass() {
+        doTestWrongPassException("7z", FileType.MULTIPLE);
+    }
+
+    @Test()
+    void extractAll_pw_prot_rar_multiple_files_inside_wrong_pass() {
+        doTestWrongPassException("rar", FileType.MULTIPLE);
     }
 }
